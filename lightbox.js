6bdb8641e6cda5a222e1bbad514c48bddf9fb126
James Holder <github.com@thirdtruck.org>
2017-04-07 22:20:34 -0400

    Fetch random images.

--- lightbox.js
+++ lightbox.js
@@ -1,6 +1,6 @@
 const PUBLIC_BETA_KEY = 'dc6zaTOxFJmzC'
-const GIPHY_PARAMS = 'limit=1&rating=g&fmt=json'
-const GIPHY_URL = `http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=${PUBLIC_BETA_KEY}&${GIPHY_PARAMS}`
+const GIPHY_PARAMS = 'rating=g'
+const GIPHY_URL = `http://api.giphy.com/v1/gifs/random?tag=funny+cat&api_key=${PUBLIC_BETA_KEY}&${GIPHY_PARAMS}`
 
 const lightboxImageEl = document.getElementById('image')
 const lightboxImageTitleEl = document.getElementById('title')
@@ -18,12 +18,12 @@ function fetchImage() {
       if (httpRequest.status === 200) {
         console.log('Success', httpRequest.responseText)
 
-        const imageData = JSON.parse(httpRequest.responseText)['data'][0]
+        const imageData = JSON.parse(httpRequest.responseText)['data']
 
         console.log('image data', imageData)
 
-        const imageTitle = imageData['slug']
-        const imageURL = imageData['images']['fixed_height']['url']
+        const imageTitle = imageData['id']
+        const imageURL = imageData['image_url']
 
         onLoadImageInfo(imageTitle, imageURL)
       } else {
