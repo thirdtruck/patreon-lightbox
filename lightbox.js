c2ebd24a286eeb99854af0410802e6c00a6e91d6
James Holder <github.com@thirdtruck.org>
2017-04-08 17:18:00 -0400

    Apply jshint feedback.

--- lightbox.js
+++ lightbox.js
@@ -1,86 +1,86 @@
 /* jshint esversion: 6 */
-const PUBLIC_BETA_KEY = 'dc6zaTOxFJmzC'
-const GIPHY_PARAMS = 'limit=1&rating=g&fmt=json'
-const GIPHY_URL = `http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=${PUBLIC_BETA_KEY}&${GIPHY_PARAMS}`
+const PUBLIC_BETA_KEY = 'dc6zaTOxFJmzC';
+const GIPHY_PARAMS = 'limit=1&rating=g&fmt=json';
+const GIPHY_URL = `http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=${PUBLIC_BETA_KEY}&${GIPHY_PARAMS}`;
 
-const lightboxImageEl = document.getElementById('image')
-const lightboxImageTitleEl = document.getElementById('title')
-const prevButton = document.getElementById('prev')
-const nextButton = document.getElementById('next')
+const lightboxImageEl = document.getElementById('image');
+const lightboxImageTitleEl = document.getElementById('title');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
 
-let offset = 0
+let offset = 0;
 
 function titleFromSlug(slug) {
-	let words = slug.split('-')
+	let words = slug.split('-');
 
 	// In case the slug has no tags in it
 	if (words.length === 1) {
-		return words[0]
+		return words[0];
 	}
 
-	words.pop() // Cut off the randomized trailing section
-	words = words.map((word) => { return word[0].toUpperCase() + word.slice(1) }) // Capitalize each word
+	words.pop(); // Cut off the randomized trailing section
+	words = words.map((word) => { return word[0].toUpperCase() + word.slice(1); }); // Capitalize each word
 
-  return words.join(' ')
+  return words.join(' ');
 }
 
 function setTitle(title) {
-  lightboxImageTitleEl.innerText = title
+  lightboxImageTitleEl.innerText = title;
 }
 
 function createImage(imageURL) {
-  lightboxImageEl.innerHTML = `<img src="${imageURL}" />`
+  lightboxImageEl.innerHTML = `<img src="${imageURL}" />`;
 }
 
 function fetchImage() {
-  const httpRequest = new XMLHttpRequest()
+  const httpRequest = new XMLHttpRequest();
 
   httpRequest.onreadystatechange = (result) => {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        const imageData = JSON.parse(httpRequest.responseText)['data'][0]
+        const imageData = JSON.parse(httpRequest.responseText).data[0];
 
-        const imageSlug = imageData['slug']
-				const imageTitle = titleFromSlug(imageSlug)
-        const imageURL = imageData['images']['fixed_width']['url']
+        const imageSlug = imageData.slug;
+				const imageTitle = titleFromSlug(imageSlug);
+        const imageURL = imageData.images.fixed_width.url;
 
-				setTitle(imageTitle)
-				createImage(imageURL)
+				setTitle(imageTitle);
+				createImage(imageURL);
       } else {
-        console.log('Error', httpRequest.readyState)
+        console.log('Error', httpRequest.readyState);
       }
     } else {
-      console.log('Loading ...')
+      console.log('Loading ...');
     }
-  }
+  };
 
-  httpRequest.open('GET', `${GIPHY_URL}&offset=${offset}`)
-  httpRequest.send()
+  httpRequest.open('GET', `${GIPHY_URL}&offset=${offset}`);
+  httpRequest.send();
 }
 
 function onClickImage(evt) {
-  fetchImage()
+  fetchImage();
 }
 
 function nextImage() {
-  offset += 1
+  offset += 1;
 
-  fetchImage()
+  fetchImage();
 }
 
 function prevImage() {
-  offset -= 1
+  offset -= 1;
 
   // Don't fetch the image unless we're actually fetching a new one
   if (offset < 0) {
-    offset = 0
+    offset = 0;
   } else {
-    fetchImage()
+    fetchImage();
   }
 }
 
-lightboxImageEl.onclick = onClickImage
-prevButton.onclick = prevImage
-nextButton.onclick = nextImage
+lightboxImageEl.onclick = onClickImage;
+prevButton.onclick = prevImage;
+nextButton.onclick = nextImage;
 
-document.addEventListener("DOMContentLoaded", () => { fetchImage() })
+document.addEventListener("DOMContentLoaded", () => { fetchImage(); });
