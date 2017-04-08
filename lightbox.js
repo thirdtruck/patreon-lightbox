0467fa5c9c8b6b59ddef980ee4a7067c44326292
James Holder <github.com@thirdtruck.org>
2017-04-07 22:06:27 -0400

    Load an image from Giphy.

---
+++ lightbox.js
@@ -0,0 +1,25 @@
+const PUBLIC_BETA_KEY = 'dc6zaTOxFJmzC'
+const GIPHY_PARAMS = 'limit=1&rating=g&fmt=json'
+const GIPHY_URL = `http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=${PUBLIC_BETA_KEY}&${GIPHY_PARAMS}`
+
+const lightboxImageEl = document.getElementById('image')
+
+const httpRequest = new XMLHttpRequest()
+
+httpRequest.onreadystatechange = (result) => {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      console.log('Success', httpRequest.responseText)
+      const imageData = JSON.parse(httpRequest.responseText)['data'][0]
+      console.log('image data', imageData)
+      lightboxImageEl.innerHTML = `<img src="${imageData['images']['fixed_height']['url']}" />`
+    } else {
+      console.log('Error', httpRequest.readyState)
+    }
+  } else {
+    console.log('Loading ...')
+  }
+}
+
+httpRequest.open('GET', GIPHY_URL)
+httpRequest.send()
