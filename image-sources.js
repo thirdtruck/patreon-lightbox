0c0fd34901b226c04ff33496ae6360b13a6f9e8a
James Holder <github.com@thirdtruck.org>
2017-04-09 16:59:10 -0400

    Move ImageSource, GiphyImageSource into new image-sources.js.

---
+++ image-sources.js
@@ -0,0 +1,98 @@
+class ImageSource {
+  constructor() {
+    this.params = { /* source-specific URL parameters go here */ };
+    this.sourceURL = 'Placeholder';
+  }
+
+  /* Subclasses overwrite this method to extract a title and image URL from their source's raw data,
+   * which they then pass to the callback.
+   */
+  onFetchImageData(data, onFetchImage) {
+    const imageTitle = 'Placeholder';
+    const imageURL = 'Placeholder';
+
+    onFetchImage(imageTitle, imageURL);
+  }
+
+  get fetchURL() {
+    let paramStrings = Object.keys(this.params).map((key) => {
+      if (Array.isArray(this.params[key])) {
+        return `${key}=${this.params[key].join('+')}`;
+      } else {
+        return `${key}=${this.params[key]}`;
+      }
+    });
+
+    paramStrings.push(this.offsetParam);
+
+    return `${this.sourceURL}?${paramStrings.join('&')}`;
+  }
+
+  /* Subclasses may have to override this */
+  get offsetParam() {
+    return `offset=${this.offset}`;
+  }
+
+  fetchImage(offset, onFetchImage) {
+    this.offset = offset;
+
+    const httpRequest = new XMLHttpRequest();
+
+    httpRequest.onreadystatechange = (result) => {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          this.onFetchImageData(httpRequest.responseText, onFetchImage);
+        } else {
+          console.log('Error', httpRequest.readyState);
+        }
+      } else {
+        console.log('Loading ...');
+      }
+    };
+
+    httpRequest.open('GET', this.fetchURL);
+    httpRequest.send();
+  }
+}
+
+class GiphyImageSource extends ImageSource {
+  constructor(apiKey, params={}) {
+    super();
+
+    this.params = {
+      limit: 1,
+      rating: 'g',
+      fmt: 'json',
+      q: ['funny', 'cat'],
+      api_key: apiKey,
+    };
+
+    Object.assign(this.params, params); // Override default params
+
+    this.sourceURL = 'http://api.giphy.com/v1/gifs/search';
+  }
+
+  static titleFromSlug(slug) {
+    let words = slug.split('-');
+
+    // In case the slug has no tags in it
+    if (words.length === 1) {
+      return words[0];
+    }
+
+    words.pop(); // Cut off the randomized trailing section
+    words = words.map((word) => { return word[0].toUpperCase() + word.slice(1); }); // Capitalize each word
+
+    return words.join(' ');
+  }
+
+  onFetchImageData(data, onFetchImage) {
+    const imageData = JSON.parse(data).data[0];
+
+    const imageSlug = imageData.slug;
+    const imageTitle = GiphyImageSource.titleFromSlug(imageSlug);
+    const imageURL = imageData.images.fixed_width.url;
+
+    onFetchImage(imageTitle, imageURL);
+  }
+}
